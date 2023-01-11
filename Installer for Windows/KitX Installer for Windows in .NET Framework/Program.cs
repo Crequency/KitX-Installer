ba863bfd5ce10f5dca18faa2e929ae02233e5429
Dynesshely <catrol@qq.com>
2023-01-11 11:59:55 +0800

    💾 Feat: 朴素的全局异常捕获

--- Installer for Windows/KitX Installer for Windows in .NET Framework/Program.cs
+++ Installer for Windows/KitX Installer for Windows in .NET Framework/Program.cs
@@ -11,40 +11,48 @@ namespace KitX_Installer_for_Windows_in.NET_Framework
         [STAThread]
         static void Main(string[] args)
         {
-            bool isUninstall = false;
-            bool silentUninstall = false;
-
-            foreach (var item in args)
+            try
             {
-                switch (item)
+                bool isUninstall = false;
+                bool silentUninstall = false;
+
+                foreach (var item in args)
                 {
-                    case "--uninstall":
-                        isUninstall = true;
-                        break;
-                    case "--silent":
-                        silentUninstall = true;
-                        break;
+                    switch (item)
+                    {
+                        case "--uninstall":
+                            isUninstall = true;
+                            break;
+                        case "--silent":
+                            silentUninstall = true;
+                            break;
+                    }
                 }
-            }
 
-            if (isUninstall)
-            {
-                if (silentUninstall)
+                if (isUninstall)
                 {
-                    UninstallForm.Uninstall();
+                    if (silentUninstall)
+                    {
+                        UninstallForm.Uninstall();
+                    }
+                    else
+                    {
+                        Application.EnableVisualStyles();
+                        Application.SetCompatibleTextRenderingDefault(false);
+                        Application.Run(new UninstallForm());
+                    }
                 }
                 else
                 {
                     Application.EnableVisualStyles();
                     Application.SetCompatibleTextRenderingDefault(false);
-                    Application.Run(new UninstallForm());
+                    Application.Run(new MainForm());
                 }
             }
-            else
+            catch (Exception e)
             {
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new MainForm());
+                MessageBox.Show($"Error -> {e.Message}\r\n{e.StackTrace}",
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
     }
