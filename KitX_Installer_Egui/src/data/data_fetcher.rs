440b0d596c35983551390d889f8d7800d7d7a74a
Dynesshely <catrol@qq.com>
2023-06-16 16:37:26 +0800

    💾 Feat(Data): Fetch string from url.

---
+++ KitX_Installer_Egui/src/data/data_fetcher.rs
@@ -0,0 +1,10 @@
+﻿extern crate reqwest;
+
+pub async fn fetch_string_async(url: String) -> String {
+    let resp = reqwest::get(url);
+    resp.await.unwrap().text().await.unwrap()
+}
+
+pub fn fetch_string(url: String) -> String {
+    reqwest::blocking::get(url).unwrap().text().unwrap()
+}
