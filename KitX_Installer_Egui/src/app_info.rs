649e57d4e2e6449564cee6706c1845d87a432aeb
Dynesshely <catrol@qq.com>
2023-06-18 00:02:28 +0800

    💾 Feat(Arguments): Imported parser for startup arguments.

---
+++ KitX_Installer_Egui/src/app_info.rs
@@ -0,0 +1,9 @@
+﻿pub enum RunMode {
+    Cli = 1,
+    Gui = 0,
+}
+
+pub struct AppInfo {
+    pub run_mode: Option<RunMode>,
+    pub version: String,
+}
--- KitX_Installer_Egui/src/main.rs
+++ KitX_Installer_Egui/src/main.rs
@@ -2,21 +2,84 @@
 
 mod data;
 mod platforms;
+
+mod app_info;
+
+use arguments::Arguments;
+use std::env;
+
+use crate::app_info::{RunMode, AppInfo};
 use crate::platforms::windows::application;
 
 fn main() {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
 
-    if cfg!(target_os = "windows") {
-        if run_gui().is_err() {
-            eprintln!("Failed to run GUI");
+    let src_args = env::args();
+    let args: Arguments = arguments::parse(src_args).unwrap();
+
+    let app_info = process_args(args);
+
+    if app_info.is_none() {
+        return;
+    }
+
+    let app_info = app_info.unwrap();
+
+    match &app_info.run_mode.as_ref().unwrap() {
+        RunMode::Gui => {
+            if run_gui(app_info).is_err() {
+                eprintln!("Failed to run GUI.");
+                println!("Failed to run GUI.");
+            }
         }
-    } else {
-        run_cli();
+        RunMode::Cli => {
+            run_cli(app_info);
+        }
     }
 }
 
-fn run_gui() -> Result<(), eframe::Error> {
+fn process_args(args: Arguments) -> Option<AppInfo> {
+    // `clia` means `command line interface argument`
+    let mut clia_run_gui = args.get::<bool>("run-gui");
+    let mut clia_run_cli = args.get::<bool>("run-cli");
+
+    // when both `run-gui` and `run-cli` are not specified, auto detect.
+    if clia_run_gui.is_none() && clia_run_cli.is_none() {
+        clia_run_gui = Some(cfg!(target_os = "windows"));
+        clia_run_cli = Some(!clia_run_gui.unwrap());
+    }
+
+    // when both `run-gui` and `run-cli` are specified to true, error and exit.
+    if clia_run_gui.unwrap_or(false) && clia_run_cli.unwrap_or(false) {
+        eprintln!("Cannot run GUI and CLI at the same time.");
+        println!("Cannot run GUI and CLI at the same time.");
+        return None;
+    }
+
+    if clia_run_gui.unwrap_or(false) || clia_run_cli.is_none() {
+        clia_run_cli = Some(!clia_run_gui.unwrap_or(false));
+    }
+
+    if clia_run_cli.unwrap_or(false) || clia_run_gui.is_none() {
+        clia_run_gui = Some(!clia_run_cli.unwrap_or(false));
+    }
+
+    let clia_run_gui = clia_run_gui.unwrap_or(false);
+    let clia_run_cli = clia_run_cli.unwrap_or(false);
+
+    let run_in_gui = (clia_run_gui || cfg!(target_os = "windows")) && !clia_run_cli;
+
+    return Some(AppInfo {
+        run_mode: if run_in_gui {
+            Some(RunMode::Gui)
+        } else {
+            Some(RunMode::Cli)
+        },
+        version: env!("CARGO_PKG_VERSION").to_string(),
+    });
+}
+
+fn run_gui(app_info: AppInfo) -> Result<(), eframe::Error> {
     let options = application::get_native_options(None);
 
     eframe::run_native(
@@ -26,7 +89,7 @@ fn run_gui() -> Result<(), eframe::Error> {
     )
 }
 
-fn run_cli() {
+fn run_cli(app_info: AppInfo) {
     println!("KitX Installer CLI");
-    println!("v0.1.0");
+    println!("v{}", app_info.version);
 }
