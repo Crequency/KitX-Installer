3aa4341bf0b3b7fea3b0009ec540019782282ac6
Dynesshely <catrol@qq.com>
2023-06-25 13:22:55 +0800

    💾 Feat(Data): Append length of `version` and `profile` to 60.

--- KitX_Installer_Egui/src/data/download_config.rs
+++ KitX_Installer_Egui/src/data/download_config.rs
@@ -6,8 +6,8 @@
 impl DownloadConfig {
     pub fn default() -> DownloadConfig {
         DownloadConfig {
-            version: "$$_!_%Version%_@_$$".to_string(),
-            profile: "$$_!_%Profile%_@_$$".to_string(),
+            version: "$$_!_%Version%_@_$$                                        #".to_string(),
+            profile: "$$_!_%Profile%_@_$$                                        #".to_string(),
         }
     }
 }
