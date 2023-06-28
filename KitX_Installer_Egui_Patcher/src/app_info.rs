b136c562e32bb16750e5291e24974e09fe29c2ad
Dynesshely <catrol@qq.com>
2023-06-28 15:44:31 +0800

    💾 Feat: Main startup progress.

---
+++ KitX_Installer_Egui_Patcher/src/app_info.rs
@@ -0,0 +1,13 @@
+﻿pub struct AppInfo {
+    pub version: String,
+    pub patch_file_path: Option<String>,
+}
+
+impl AppInfo {
+    pub fn default() -> AppInfo {
+        AppInfo {
+            version: env!("CARGO_PKG_VERSION").to_string(),
+            patch_file_path: None,
+        }
+    }
+}
--- KitX_Installer_Egui_Patcher/src/main.rs
+++ KitX_Installer_Egui_Patcher/src/main.rs
@@ -1,3 +1,25 @@
+mod app_info;
+
+use std::env;
+
+use crate::app_info::AppInfo;
+
 fn main() {
-    println!("Hello, world!");
+    let args = arguments::parse(env::args()).unwrap();
+
+    let mut app_info = AppInfo::default();
+
+    app_info.patch_file_path = args.get::<String>("patch");
+
+    println!("KitX Installer Patcher");
+    println!("Version: v{}", app_info.version);
+
+    if app_info.patch_file_path.is_none() {
+        println!("Please provide file to patch path !");
+        return;
+    }
+
+    println!("File to patch: {:?}", app_info.patch_file_path);
+
+
 }
