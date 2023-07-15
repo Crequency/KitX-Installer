cb7f846f2f2ee6e57d12168a5634cacc7c0e1c5a
Dynesshely <catrol@qq.com>
2023-07-15 09:53:17 +0800

    📝 Chore(build.rs): Set to require administrator permission.

--- KitX_Installer_Egui/build.rs
+++ KitX_Installer_Egui/build.rs
@@ -5,6 +5,25 @@ fn main() {
         println!("Running task from `winres` to set icon and manifest.");
         let mut res = winres::WindowsResource::new();
         res.set_icon("./assets/icon.ico");
+        res.set_manifest(
+            r#"
+            <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+                <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+                    <security>
+                        <requestedPrivileges>
+                            <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
+                        </requestedPrivileges>
+                    </security>
+                </trustInfo>
+                <application xmlns="urn:schemas-microsoft-com:asm.v3">
+                    <windowsSettings>
+                        <dpiAware xmlns="http://schemas.microsoft.com/SMI/2005/WindowsSettings">true</dpiAware>
+                        <longPathAware xmlns="http://schemas.microsoft.com/SMI/2016/WindowsSettings">true</longPathAware>
+                    </windowsSettings>
+                </application>
+            </assembly>
+            "#,
+        );
         res.compile().unwrap();
 
         println!("Running `static_vcruntime::metabuild()`.");
