db730f978db7e9bc5df5357b50ecbc3ff628fb4b
Dynesshely <catrol@qq.com>
2024-02-04 02:49:29 +0800

    📝 Chore(tools-chain): Release script.

---
+++ KitX_Installer_Egui/release.ps1
@@ -0,0 +1,3 @@
+﻿cargo build --release
+Copy-Item './target/release/kitx_installer_egui.exe' './target/release/kitx_installer_egui_lzma-compressed.exe'
+upx --best --lzma './target/release/kitx_installer_egui_lzma-compressed.exe'
